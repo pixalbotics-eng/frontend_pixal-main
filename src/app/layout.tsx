c5b501851348f9f773979504a2ab486e6621883c
pixalbotics-eng <pixalbotics@gmail.com>
2026-07-26 09:12:35 +0500

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -179,11 +179,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </AuthProvider>
         <Analytics />
 
-<script
-  src="https://d16o8s09pifhtc.cloudfront.net/widget/pixalbotics-dev-test/pixalbotic.js"
-  defer
-  data-api-origin="https://api.convergeit.app"
-></script>
+<script src="https://cdn.convergeit.app/widget/pixalbotics/pixal-tech/pixalbotics/assetsloader.js" defer></script>
+
       </body>
     </html>
   );
